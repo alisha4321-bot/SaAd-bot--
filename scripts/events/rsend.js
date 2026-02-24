799b2752215a54983578092fb2dd55ad708b87a4
saadxitachi <saad2026q@gmail.com>
2026-02-24 17:07:02 +0600

    Update rsend.js

--- scripts/events/rsend.js
+++ scripts/events/rsend.js
@@ -1,4 +1,4 @@
-.event install rsend.js const fs = require("fs-extra");
+const fs = require("fs-extra");
 const axios = require("axios");
 const path = require("path");
 
@@ -6,7 +6,7 @@ module.exports = {
   config: {
     name: "rsend",
     version: "3.0",
-    author: "SaAd / Gemini",
+    author: "SaAd & Gemini",
     description: "Recover unsent messages and media"
   },
 
@@ -32,7 +32,7 @@ module.exports = {
 
       try {
         const name = await usersData.getName(senderID) || "Someone";
-        let msgBody = `sir 🐸 ${name}, delete a massage \n\n${savedMsg.body ?`#: ${savedMsg.body}` : ""}`;
+        let msgBody = `Sir 🐸🙏 ${name}, delete a massage\n\n${savedMsg.body ?`#: ${savedMsg.body}` : ""}`;
 
         const streams = [];
         const cacheDir = path.join(__dirname, "cache");
