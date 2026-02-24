6132780b52d27add943e8b8956e665223ba21e9a
saadxitachi <saad2026q@gmail.com>
2026-02-24 22:39:26 +0600

    Update notification.js

--- scripts/cmds/notification.js
+++ scripts/cmds/notification.js
@@ -31,7 +31,7 @@ module.exports = {
 		},
 		en: {
 			missingMessage: "⚠️ Please enter the message you want to send to all groups.",
-			notification: "╭─  🔔 NOTIFICATION 🔔   ─╮\n│ ✨ FROM : ADMIN SaAd\n╰────────────╯",
+			notification: "╭─  🔔 NOTIFICATION 🔔   ─╮\n│ ✨ FROM : ADMIN SaAd\n╰──────_──────╯",
 			sendingNotification: "🚀 Start sending notification to %1 chat groups...",
 			sentNotification: "✅ Sent notification to %1 groups successfully!",
 			errorSendingNotification: "❌ An error occurred while sending to %1 groups:\n%2"
